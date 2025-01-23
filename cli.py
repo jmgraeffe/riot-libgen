07261c3084b6caa844a976730025ef6ed7a5db69
Jon-Mailes Graeffe <mail@jonni.it>
2025-01-23 15:17:45 +0100

    added support for multiple config files at once

--- cli.py
+++ cli.py
@@ -3,12 +3,11 @@ from argparse import ArgumentParser
 
 import riot_libgen
 
-#TODO properly add support for multiple configs
 #TODO error handling (config exists etc.)
 
 if __name__ == '__main__':
 	parser = ArgumentParser(description="Command-Line Interface of the RIOT OS Library Generator")
-	parser.add_argument('-c', '--config', type=str, required=True, help='Path of the YAML specification file')
+	parser.add_argument('-c', '--config', type=str, required=True, nargs='+', help='Path(s) of the YAML specification file')
 	parser.add_argument('-d', '--dest', type=str, required=True, help='Path to generate the RIOT OS module to')
 	parser.add_argument('-g', '--generator', choices=['wasm', 'js'], required=True, help='Generator to use (currently supported: "wasm", "js")')
 	args = parser.parse_args()
@@ -21,5 +20,7 @@ if __name__ == '__main__':
 	else:
 		print('"{}" is not a supported generator. Currently supported: "wasm", "js"')
 		exit(1)
-	libgen.load_config_from_file(args.config)
+	for config in args.config:
+		print(config)
+		libgen.load_config_from_file(config)
 	libgen.render(Path(args.dest))
